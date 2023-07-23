0b02ad922062736978f412769f0530b0941500aa
tejasvi8686 <tejasviraj8686@gmail.com>
2023-07-23 15:52:45 +0530

    added SearchItems

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,8 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
+import SearchItem from "../../components/searchItem/SearchItem";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const List = () => {
   const location = useLocation();
@@ -87,7 +89,17 @@ const List = () => {
             </div>
             <button>Search</button>
           </div>
-          <div className="listResult"></div>
+          <div className="listResult">
+          <SearchItem />
+          <SearchItem />
+          <SearchItem />
+          <SearchItem />
+          <SearchItem />
+          <SearchItem />
+          <SearchItem />
+          <SearchItem />
+          <SearchItem />
+          </div>
         </div>
       </div>
     </div>
